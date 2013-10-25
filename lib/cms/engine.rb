22ff50303b965cd146cc80353de82c57e0d2beb7
Nick Herro <ncherro@gmail.com>
2013-10-25 17:01:30 -0400

    updated engine

--- lib/cms/engine.rb
+++ lib/cms/engine.rb
@@ -1,5 +1,28 @@
+require "spree/core"
+require "nested_form"
+require "simple_form"
+require "ancestry"
+require "rack/cache"
+require "dragonfly"
+require "tinymce-rails"
+
 module Cms
   class Engine < ::Rails::Engine
+
     isolate_namespace Cms
+
+    # precompile our css / js files
+    initializer "precompile", :group => :all do |app|
+      app.config.assets.precompile << "tinymce/plugins/cms_image/plugin.js"
+      app.config.assets.precompile << "admin/spree_cms_image.js"
+      app.config.assets.precompile << "tinymce/plugins/cms_file/plugin.js"
+      app.config.assets.precompile << "admin/spree_cms_file.js"
+    end
+
+    # use rspec for tests
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
   end
 end
